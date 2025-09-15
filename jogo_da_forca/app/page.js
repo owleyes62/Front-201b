f86e891547209b07a721f9c97689d1fd5fccef82
Iwerson <109252776+owleyes62@users.noreply.github.com>
2025-09-15 23:45:16 +0000

    commit tudo fodase

---
+++ jogo_da_forca/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="home-page">
+      <h1 className="home-title">
+        Site criado para a disciplina Programação Front-End
+      </h1>
+    </div>
+  );
+}
